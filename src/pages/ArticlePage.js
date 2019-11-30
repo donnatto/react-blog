75029c5704bf003bbb1dbe50ac2d736f32454aee
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-29 19:45:59 -0500

    add article links functionality

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import articleContent from './article-content';
 
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find(article => article.name === name);
 
-const ArticlePage = () => (
-  <>
-    <h1>This is an Article</h1>
-  </>
-)
+  if (!article) {
+    return <h1>Article does not exist!</h1>
+  }
+
+  return (
+    <>
+      <h1>{article.name}</h1>
+      {article.content.map((paragrapgh, key) => (
+        <p key={key}>{paragrapgh}</p>
+      ))}
+    </>
+  );
+};
 
 export default ArticlePage;
