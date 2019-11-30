f55801468f9d8e0a9fc06cd7f4696c3c6e8f9aec
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-30 11:25:47 -0500

    connect front to backend, add proxy and fetch article info

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
@@ -7,8 +7,23 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
+  const [articleInfo, setArticleInfo] = useState({
+    upvotes: 0,
+    comments: []
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    }
+    fetchData();
+  }, [name]);
+
   if (!article) {
-    return <NotFoundPage />
+    return <NotFoundPage />;
   }
 
   const otherArticles = articleContent.filter(article => article.name !== name);
@@ -16,6 +31,7 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.name}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragrapgh, key) => (
         <p key={key}>{paragrapgh}</p>
       ))}
