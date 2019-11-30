0a3ade0bf720ad2582ef8703a9bd7b8cf76f5a34
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-29 20:24:06 -0500

    add notfoundpage.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/HomePage';
@@ -9,6 +10,7 @@ import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 function App() {
@@ -17,10 +19,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={Homepage} exact />
-          <Route path="/about" component={AboutPage} exact />
-          <Route path="/articles-list" component={ArticlesListPage} exact />
-          <Route path="/article/:name" component={ArticlePage} exact />
+          <Switch>
+            <Route path="/" component={Homepage} exact />
+            <Route path="/about" component={AboutPage} exact />
+            <Route path="/articles-list" component={ArticlesListPage} exact />
+            <Route path="/article/:name" component={ArticlePage} exact />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
